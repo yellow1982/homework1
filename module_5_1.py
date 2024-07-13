7415a576148f40779552b0e7666433e51293812e
Илья <Yellow82@inbox.ru>
2024-07-13 15:36:15 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -4,7 +4,7 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
-        if 2 > new_floor or new_floor > self.number_of_floors:
+        if 1 > new_floor or new_floor > self.number_of_floors:
             print("Такого этажа не существует")
         else:
             floor = 1
