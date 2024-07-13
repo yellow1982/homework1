587e64449333552d4419180653e44687329bfafb
Илья <Yellow82@inbox.ru>
2024-07-13 16:06:33 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -1,12 +1,98 @@
-class Building:
-    total = 0
+class House:
+    def __eq__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors == other.number_of_floors:
+                return True
+            else:
+                return False
 
-    def __init__(self):
-        Building.total += 1
+    def __lt__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors < other.number_of_floors:
+                return True
+            else:
+                return False
 
+    def __le__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors <= other.number_of_floors:
+                return True
+            else:
+                return False
 
-while Building.total < 40:
-    name_bilding = "bild_" + str(Building.total)
-    name_bilding = Building()
-    print(name_bilding)
+    def __gt__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors > other.number_of_floors:
+                return True
+            else:
+                return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors >= other.number_of_floors:
+                return True
+            else:
+                return False
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors != other.number_of_floors:
+                return True
+            else:
+                return False
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 > new_floor or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            floor = 1
+            while floor <= new_floor:
+                print(floor)
+                floor += 1
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
 
