13f5fedcc81cdc2f7f1bbf02ea86b4e1e3acfbae
Илья <Yellow82@inbox.ru>
2024-07-22 20:42:56 +0500

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,40 @@
+from pprint import pprint
+
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        f = open(self.__file_name, 'r')
+        return f.read()
+
+    def add(self, *products):
+        for i in products:
+            if i.name in self.get_products():
+                print(f"Продукт {i.name} уже есть в магазине")
+            else:
+                f = open(self.__file_name, 'a')
+                f.write(f'{i}\n')
+                f.close()
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
