4b615b9b011a00e07d5bad5ea6bcd058c976609f
Илья <Yellow82@inbox.ru>
2024-07-23 09:13:01 +0500

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -21,15 +21,11 @@ class Shop:
     def add(self, *products):
         prod_in_list = True
         for i in products:
-            print("i.name = ", i.name)
             if not self.get_products().splitlines():
-                print("пустой список, делаем первую запись", i)
                 f = open(self.__file_name, 'a')
                 f.write(f'{i}\n')
-                print("Добавился в список", self.get_products().splitlines())
                 f.close()
             for j in self.get_products().splitlines():
-                print("j = ", j, "i.name = ", i.name)
                 if j.startswith(i.name):
                     prod_in_list = True
                     print(f"Продукт {i.name} уже есть в магазине")
@@ -39,7 +35,6 @@ class Shop:
             if not prod_in_list:
                 f = open(self.__file_name, 'a')
                 f.write(f'{i}\n')
-                print("Добавился в список", self.get_products().splitlines())
                 f.close()
                 break
 
