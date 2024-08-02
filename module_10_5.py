9ec1d99f59d73188e2399fa0f43e2a564bd31600
Илья <Yellow82@inbox.ru>
2024-08-02 13:19:00 +0500

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,39 @@
+import multiprocessing
+
+
+class WarehouseManager:
+    data = {}
+
+    def process_request(self, request):
+        if request[1] == "receipt":
+            self.data[request[0]] = request[2]
+            print(self.data)
+        if request[1] == "shipment":
+            for item in self.data.items():
+                if item[0] == request[0]:
+                    self.data[item[0]] = item[1] - request[2]
+                    print(self.data)
+
+    def run(self, requests):
+        with multiprocessing.Pool(processes=5) as pool:
+            pool.map(self.process_request, requests)
+
+
+if __name__ == '__main__':
+    # Создаем менеджера склада
+    manager = WarehouseManager()
+
+    # Множество запросов на изменение данных о складских запасах
+    requests = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50)
+    ]
+
+    # Запускаем обработку запросов
+    manager.run(requests)
+
+    # Выводим обновленные данные о складских запасах
+    print(manager.data)
