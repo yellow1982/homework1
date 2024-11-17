199adb0cbbbdb4738e8d6f492c1e361bc53ff12b
Илья <Yellow82@inbox.ru>
2024-11-17 10:09:20 +0500

    Add files via upload

---
+++ module_16_5.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+users = []
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+
+@app.get("/")
+def get_all_users(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse("users.html", {"request": request, "users": users})
+
+
+@app.get('/user/{user_id}')
+def get_user(request: Request, user_id: int) -> HTMLResponse:
+    user = next((user for user in users if user.id == user_id), None)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return templates.TemplateResponse("users.html", {"request": request, "user": user})
+
+
+@app.post("/user/{username}/{age}")
+def add_user(user: User, username: str, age: int):
+    if not users:
+        user.id = 1
+    else:
+        user.id = len(users) + 1
+    user.username = username
+    user.age = age
+    users.append(user)
+    return user
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+def update_user(user_id: int, username: str, age: int):
+    try:
+        edit_user = users[user_id - 1]
+        edit_user.username = username
+        edit_user.age = age
+        return edit_user
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete('/user/{user_id}')
+def delete_user(user_id: int):
+    try:
+        deleted_user = users.pop(user_id - 1)
+        return deleted_user
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
